1d51ec4f0f6a74fc7ac76bb66436f3aed1b19887
meerbahadin <meerbahadin10@gmail.com>
2025-08-20 17:38:59 +0300

    Remove animated background component and simplify link styles
    Eliminates the AnimatedBackground component to reduce complexity and improve maintainability. Updates the Links component to apply simpler hover and active styles directly, removing the need for animation logic. Enhances performance and simplifies the codebase by removing an unused file.

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { AnimatedBackground } from '@/components/motion/animated-background'
 import { cn } from '../lib/utils'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
@@ -13,30 +12,20 @@ export function Links() {
   ]
 
   return (
-    <div className='flex flex-row select-none'>
-      <AnimatedBackground
-        className='rounded-lg bg-white/10'
-        transition={{
-          type: 'spring',
-          bounce: 0.2,
-          duration: 0.3,
-        }}
-        enableHover
-      >
-        {TABS.map((tab) => (
-          <Link
-            href={tab.href}
-            key={tab.href}
-            data-id={tab.href}
-            className={cn(
-              'px-2 py-0.5 text-zinc-300 transition-colors duration-200 cursor-pointer text-sm',
-              pathname === tab.href && 'text-zinc-100 font-semibold'
-            )}
-          >
-            {tab.label}
-          </Link>
-        ))}
-      </AnimatedBackground>
+    <div className='flex flex-row select-none gap-2'>
+      {TABS.map((tab) => (
+        <Link
+          href={tab.href}
+          key={tab.href}
+          data-id={tab.href}
+          className={cn(
+            'px-2 py-0.5 text-zinc-300 hover:text-zinc-200 rounded-lg bg-transparent hover:bg-zinc-800 transition-colors duration-200 cursor-pointer text-sm',
+            pathname === tab.href && 'text-zinc-100 font-semibold'
+          )}
+        >
+          {tab.label}
+        </Link>
+      ))}
     </div>
   )
 }
