2a55716f2342c931f431a78ee61d031586027ac3
meerbahadin <meerbahadin10@gmail.com>
2025-08-20 17:40:12 +0300

    Improve link hover styling and simplify container layout
    Updates the hover background color of links to enhance visual contrast and modernize the design. Removes unnecessary gap styling in the container for a cleaner layout.

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -12,14 +12,14 @@ export function Links() {
   ]
 
   return (
-    <div className='flex flex-row select-none gap-2'>
+    <div className='flex flex-row select-none'>
       {TABS.map((tab) => (
         <Link
           href={tab.href}
           key={tab.href}
           data-id={tab.href}
           className={cn(
-            'px-2 py-0.5 text-zinc-300 hover:text-zinc-200 rounded-lg bg-transparent hover:bg-zinc-800 transition-colors duration-200 cursor-pointer text-sm',
+            'px-2 py-0.5 text-zinc-300 hover:text-zinc-200 rounded-lg bg-transparent hover:bg-white/10 transition-colors duration-200 cursor-pointer text-sm',
             pathname === tab.href && 'text-zinc-100 font-semibold'
           )}
         >
